74672b5b007155aeb163a40168b7741873787b99
Alex Kamedov <alex@kamedov.ru>
2012-02-25 16:27:42 +0600

    fix

--- cas_provider/urls.py
+++ cas_provider/urls.py
@@ -5,6 +5,5 @@ urlpatterns = patterns('cas_provider.views',
     url(r'^login/?$', 'login', name='cas_login'),
     url(r'^validate/?$', 'validate', name='cas_validate'),
     url(r'^serviceValidate/?$', 'service_validate', name='cas_service_validate'),
-    url(r'^proxyValidate/?$', 'service_validate', name='cas_proxy_validate'),
     url(r'^logout/?$', 'logout', name='cas_logout'),
 )
